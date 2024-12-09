5197c6c438c7a6da31bf1a60021bc4ab9ef333bf
proxythegod1234 <proxy@evilmail.to>
2024-12-09 16:51:14 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,17 +2,13 @@ const overlay = document.getElementById("overlay");
 const content = document.querySelector(".content");
 const backgroundVideo = document.getElementById("background-video");
 
-
 backgroundVideo.volume = 0.3;
 
 function playMusicAndHideOverlay() {
- 
     backgroundVideo.style.display = 'block';
-    backgroundVideo.play(); 
-    
+    backgroundVideo.play();
 
     overlay.classList.add("hidden");
-
 
     setTimeout(() => {
         overlay.style.display = "none";
@@ -21,11 +17,9 @@ function playMusicAndHideOverlay() {
     }, 500);
 }
 
-
 document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
 });
-
 
 document.querySelectorAll(".profile-box").forEach(box => {
     box.addEventListener("mousemove", (e) => {
@@ -49,13 +43,12 @@ document.querySelectorAll(".profile-box").forEach(box => {
     });
 });
 
-
-document.body.addEventListener("mousemove", (event) => {
-    createSpark(event.clientX, event.clientY);
+document.body.addEventListener("mousemove", function (event) {
+    createFirework(event.clientX, event.clientY);
 });
 
-function createSpark(x, y) {
-    const numSparks = 5;
+function createFirework(x, y) {
+    const numSparks = 10;
     for (let i = 0; i < numSparks; i++) {
         const spark = document.createElement("div");
         spark.classList.add("spark");
@@ -77,36 +70,3 @@ function createSpark(x, y) {
         }, duration * 1000);
     }
 }
-
-function playVideoAndHideOverlay() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.display = 'none';
-
-
-    const background = document.getElementById('background-video');
-    background.style.display = 'block';
-
-
-    const content = document.querySelector(".content");
-    content.style.display = "block";
-    showSnowflakes();
-}
-
-
-setTimeout(() => {
-    document.getElementById('script-container').style.opacity = 1;
-}, 3000);
-
-
-setTimeout(() => {
-    const container = document.getElementById('background-video');
-    if (container) {
-        container.style.opacity = 0;
-        container.style.visibility = 'visible';
-        container.style.transition = 'opacity 2s ease-in-out'; 
-
-        setTimeout(() => {
-            container.style.opacity = 1;
-        }, 100);
-    }
-}, 3000);
