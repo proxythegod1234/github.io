5396c387814c2a30b91deb76fbf5e6c97bf6553b
proxythegod1234 <proxy@evilmail.to>
2025-01-01 00:42:26 -0500

    Update card.js

--- assets/js/card.js
+++ assets/js/card.js
@@ -46,26 +46,7 @@ $cards.on("mouseout", function () {
     transform: "rotateX(0deg) rotateY(0deg)"
   });
 
-  document.querySelector("#loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
 
-    window.onload = function() {
-      document.querySelector('.loading-screen').style.display = 'none';
-      document.querySelector('.login-container').style.display = 'flex';
-      
-   
-      document.querySelector('#extraButton').addEventListener('click', function() {
-          window.location.href = 'card.html';
-      });
-  };
-
-    const username = document.querySelector("#username").value;
-    if (username) {
-        window.location.href = "desktop.html";
-    } else {
-        alert("Please select a username.");
-    }
-});
   $cards.removeClass("active");
   $style.html("");
 });
